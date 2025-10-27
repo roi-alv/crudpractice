3392c386ffb59bd4826fd68ff637ff6d230c10a3
Roi Alvarico (NCS) <roi.alvarico@ph.ncs-i.com>
2025-10-28 07:43:59 +0800

    feat: added user service & fix: user getter/setter

--- src/main/java/com/example/crudpractice/model/user.java
+++ src/main/java/com/example/crudpractice/model/user.java
@@ -16,4 +16,30 @@ public class user {
     public String name;
     public String email;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+
 }
---
+++ src/main/java/com/example/crudpractice/service/UserService.java
@@ -0,0 +1,37 @@
+package com.example.crudpractice.service;
+
+import com.example.crudpractice.model.user;
+import com.example.crudpractice.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<user> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    public user createUser(user user){
+        return userRepository.save(user);
+    }
+
+    public user updateUser(Long id, user updatedUser){
+        return userRepository.findById(id).map(user ->{
+            user.setName(updatedUser.getName());
+            user.setEmail(updatedUser.getEmail());
+            return userRepository.save(user);
+        }).orElseThrow(() -> new NoSuchElementException("User not Found with id:" + id));
+    }
+
+    public void deleteUser(Long id){
+        userRepository.deleteById(id);
+    }
+}
