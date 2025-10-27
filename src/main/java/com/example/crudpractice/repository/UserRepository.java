556be63eb79374ce59505e921de8c84889d68c7f
Roi Alvarico (NCS) <roi.alvarico@ph.ncs-i.com>
2025-10-28 07:31:23 +0800

    feat: added UserRepository

---
+++ src/main/java/com/example/crudpractice/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.crudpractice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.crudpractice.model.user;
+
+public interface UserRepository extends JpaRepository<user, Long> {
+}
