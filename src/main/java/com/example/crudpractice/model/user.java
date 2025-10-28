f75719581427a4e35bdcdcde1eea68ecc2d63500
Roi Alvarico (NCS) <roi.alvarico@ph.ncs-i.com>
2025-10-28 08:02:02 +0800

    fix: table generation for user

--- src/main/java/com/example/crudpractice/model/user.java
+++ src/main/java/com/example/crudpractice/model/user.java
@@ -1,12 +1,10 @@
 package com.example.crudpractice.model;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class user {
 
     @Id
